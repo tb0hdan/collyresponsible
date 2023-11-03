68e2f005b902867881a0d33c61b32a51cb19cebc
Bohdan Turkynewych <tb0hdan@gmail.com>
2023-11-02 20:55:01 -0400

    Added MaxRuntime

--- examples/main.go
+++ examples/main.go
@@ -13,9 +13,10 @@ func main() {
 	httpCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	profile := &collyresponsible.CrawlerProfile{
-		Ctx:       httpCtx,
-		Website:   "https://en.wikipedia.org",
-		UserAgent: "Mozilla/5.0 (compatible; Colly Responsible; +https://github.com/tb0hdan/collyresponsible)",
+		Ctx:        httpCtx,
+		Website:    "https://en.wikipedia.org",
+		UserAgent:  "Mozilla/5.0 (compatible; Colly Responsible; +https://github.com/tb0hdan/collyresponsible)",
+		MaxRuntime: 3600,
 		ResponseHooks: []func(r *colly.Response){
 			func(r *colly.Response) {
 				fmt.Printf("URL: %s\nHeaders: %s\nBody length: %d\n", r.Ctx.Get("url"), r.Headers, len(r.Body))
